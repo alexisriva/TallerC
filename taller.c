76c2455030deb692412ead7c1d512a82ce19646c
Alexis Rivadeneira <alexisleo15@gmail.com>
2017-05-27 03:00:15 -0500

    parte2.2

--- taller.c
+++ taller.c
@@ -8,6 +8,17 @@
 //Se usan los codigos ASCII de las letras para la resolucion de este problema
 
 void cifrado(char mensaje[], int n);
+int getIndice(char letra);
+
+char abc[27] ={'a','b','c','d','e','f','g','h','i','j','k','l','m','n',
+		       'o','p','q','r','s','t','u','v','w','x','y','z',' '};
+char morseL[27][13]={{". ---"},{"--- . . ."},{"--- . --- ."},{"--- . ."},{"."},{". . --- ."},
+		     {"--- --- ."},{". . . ."},{". ."},{". --- --- ---"},{"--- . ---"},{". --- . ."},   	             {"--- ---"},{"--- ."},{"--- --- ---"},{". --- --- ."},{"--- --- . ---"},
+		     {". --- ."},{". . ."},{"---"},{". . ---"},{". . . ---"},{". --- ---"},
+		     {"--- . . ---"},{"--- . --- ---"},{"--- --- . ."},{"/"}};
+char morseN[10][5] = {{"--- --- --- --- ---"},{". --- --- --- ---"},{". . --- --- ---"},
+		      {". . . --- ---"},{". . . . ----"},{". . . . ."},{"--- . . . ."},
+		      {"--- --- . . ."},{"--- --- --- . ."},{"--- --- --- --- ."}};
 
 int main(int argc, char *argv[]) {
 	
@@ -39,6 +50,7 @@ int main(int argc, char *argv[]) {
 void cifrado(char mensaje[], int n) {
 
         char output[LARGO] = {0};
+	char outputMorse[LARGO] = {0};
         int ascii;
         int nuevo_ascii;
 
@@ -72,10 +84,25 @@ void cifrado(char mensaje[], int n) {
 	                                        output[i] = nuevo_ascii;
 	                        }
 	                }
-	        } else
+			strcat(outputMorse,morseL[getIndice(tolower(output[i]))]);
+	        } else if (mensaje[i] == ' ') {
 			output[i] = mensaje[i];
+			strcat(outputMorse,morseL[getIndice(output[i])]);
+		} else
+			output[i] = mensaje[i];
+		strcat(outputMorse,"   ");
         }
 
         printf("Tu mensaje cifrado es: %s\n",output);
+	printf("El mensaje en Morse es: %s\n",outputMorse);
 
+}
+
+int getIndice(char letra) {
+	int indice = -1;
+	for (int i=0;i<27;i++) {
+		if (abc[i]==letra)
+			indice = i;
+	}
+	return indice;
 }
