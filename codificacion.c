a09ad34419709c8e611875c8b38fb24c0a2e1b3e
Vivi <vlaurido@espol.edu.ec>
2017-06-05 14:46:34 -0500

    Implementada funcion morse

--- codificacion.c
+++ codificacion.c
@@ -1,3 +1,31 @@
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#define LARGO 1024
+
+char abc[27] ={'a','b','c','d','e','f','g','h','i','j','k','l','m','n',
+'o','p','q','r','s','t','u','v','w','x','y','z',' '};
+char morseL[27][13]={{". ---"},{"--- . . ."},{"--- . --- ."},{"--- . ."},{"."},{". . --- ."},
+		     {"--- --- ."},{". . . ."},{". ."},{". --- --- ---"},{"--- . ---"},{". --- . ."},   	             {"--- ---"},{"--- ."},{"--- --- ---"},{". --- --- ."},{"--- --- . ---"},
+		     {". --- ."},{". . ."},{"---"},{". . ---"},{". . . ---"},{". --- ---"},
+		     {"--- . . ---"},{"--- . --- ---"},{"--- --- . ."},{"/"}};
+char morseN[10][5] = {{"--- --- --- --- ---"},{". --- --- --- ---"},{". . --- --- ---"},
+		      {". . . --- ---"},{". . . . ----"},{". . . . ."},{"--- . . . ."},
+{"--- --- . . ."},{"--- --- --- . ."},{"--- --- --- --- ."}};
+
 char* claveMorse(char* mensaje){
-	/*CODIGO ALEXIS*/
+	char* outputMorse = (char *) malloc(sizeof(char) * LARGO);
+	
+	for (int i=0;i<strlen(mensaje);i++) {
+		for (int j=0;j<27;j++) {
+			if (mensaje[i] == ' '){
+				strcat(outputMorse,morseL[i]);
+			}			
+			else if (tolower(mensaje[i])==abc[j]){
+				strcat(outputMorse,morseL[j]);			
+			}	
+			strcat(outputMorse,"   ");
+		}
+	}
+	return outputMorse;
 }
