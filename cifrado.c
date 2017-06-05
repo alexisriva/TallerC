d21d05761994c5b9dff57ea28cc6f67f5ecf3eb7
Vivi <vlaurido@espol.edu.ec>
2017-06-05 13:58:22 -0500

    Estan todas las funciones implementadas en cifrado.c

--- cifrado.c
+++ cifrado.c
@@ -1,33 +1,18 @@
-char* cifradoCiclico(char* mensaje, int llave){
-	char alfabeto[26];
-	/* llenando el arreglo alfabeto */
-	alfabeto[0] = 'a';
-	alfabeto[1] = 'b';
-	alfabeto[2] = 'c';
-	alfabeto[3] = 'd';
-	alfabeto[4] = 'e';
-	alfabeto[5] = 'f';
-	alfabeto[6] = 'g';
-	alfabeto[7] = 'h';
-        alfabeto[8] = 'i';
-        alfabeto[9] = 'j';
-        alfabeto[10] = 'k';
-        alfabeto[11] = 'l';
-        alfabeto[12] = 'm';
-        alfabeto[13] = 'n';
-	alfabeto[14] = 'o';
-        alfabeto[15] = 'p';
-        alfabeto[16] = 'q';
-        alfabeto[17] = 'r';
-        alfabeto[18] = 's';
-        alfabeto[19] = 't';
-        alfabeto[20] = 'u';
-	alfabeto[21] = 'v';
-        alfabeto[22] = 'w';
-        alfabeto[23] = 'x';
-        alfabeto[24] = 'y';
-        alfabeto[25] = 'z';
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#define FILAS 26
+#define COLS 26
+#define LARGO 1024
 
+char alfabeto[26]={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','w','x','y','z'};
+int getIndice(char letra); //Funcion auxiliar
+
+char* cifradoCiclico(char* mensaje, int llave){
+	int lenMen;
+	char caracter = '\0';	
+		
 	lenMen = 0;
 	while(mensaje[lenMen]!='\0'){
 		lenMen++;
@@ -49,12 +34,72 @@ char* cifradoCiclico(char* mensaje, int llave){
 		}
 	}
 	return mensaje;
-}
+} //FIN CIFRADO CICLICO
 
 char* cifradoAutollave(char* mensaje, char* llave){
-	/*CODIGO ALEXIS*/
+	char tabula_recta[FILAS][COLS];
+	int pos,row,col;
+	char* cifrado = (char *) malloc(sizeof(char) * LARGO);
+	char mensajec[LARGO] = {0};
+	char psw[LARGO] = {0};
+	
+	strcpy(psw,llave);
+	
+	for (row=0;row<FILAS;row++) {
+		pos=0;
+		for (col=0;col<COLS;col++) {
+			if ('A'+row+col>'Z' && row>0) {
+				tabula_recta[row][col]='A'+pos;
+				pos++;
+			}
+			else if ('A'+row+col=='Z')
+				tabula_recta[row][col]='Z';
+			else
+				tabula_recta[row][col]='A'+row+col;
+		}
+	}
+	
+	for (int i=0;i<strlen(mensaje);i++) {
+		if (mensaje[i]==' ')
+			continue;
+		mensajec[i]=mensaje[i];
+	}
+	
+	strcat(psw,mensajec);
+	
+	for (int i=0;i<strlen(mensajec);i++) {
+		int imensajec = getIndice(mensajec[i]);
+		int ipsw = getIndice(psw[i]);
+		cifrado[i] = tabula_recta[imensajec][ipsw];
+	}
+	
+	return cifrado;
+} //FIN CIFRADO AUTOLLAVE
+
+//Metodo auxiliar usado en cifrado autollave
+int getIndice(char letra) {
+	int indice = -1;
+	for (int i=0;i<27;i++) {
+		if (alfabeto[i]==letra)
+			indice = i;
+	}
+	return indice;
 }
 
 char* cifradoContrasena(char* mensaje, char* llave){
-	/*CODIGO ALEXIS*/
-}
+	char* cifrado = (char *) malloc(sizeof(char) * LARGO);
+	char psw[LARGO] = {0};
+	
+	strcpy(psw,llave);
+	strcat(psw,alfabeto);
+	
+	for (int i=0;i<strlen(mensaje);i++) {
+		for (int j=0;j<26;j++) {
+			if (tolower(mensaje[i])==alfabeto[j])
+				cifrado[i]=psw[j];
+			else
+				cifrado[i]=mensaje[i];
+		}
+	}
+	return cifrado;
+} //FIN CIFRADO CONTRASENA
